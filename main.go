d24290d439cb34c25ad13938b9b348d78b328b20
Vincent Hellot <hellvinz@gmail.com>
2012-02-13 11:51:13 +0100

    push service to graphite via a chan

--- main.go
+++ main.go
@@ -84,15 +84,46 @@ type Monit struct {
   Service []Service
 }
 
-func SendToGraphite(metric string, value string, timestamp int64) {
-  client, err := net.Dial("tcp", "localhost:2003")
+type Graphite struct {
+  conn net.Conn
+}
+
+var serviceq chan *Service
+
+func (graphite *Graphite) Setup(addr string) {
+  log.Println("starting")
+  conn, err := net.Dial("tcp", addr)
+  graphite.conn = conn
   if err != nil {
     log.Fatal(err)
   }
-  defer client.Close()
+  log.Println("dialed")
+  defer conn.Close()
+  serviceq = make(chan *Service)
+  for {
+    service := <-serviceq
+    log.Println("got service!")
+    if service.Type == 5 {
+      continue
+    }
+    go graphite.Send(service.Name+".status", strconv.Itoa(service.Status), service.Collected_Sec)
+    go graphite.Send(service.Name+".monitor", strconv.Itoa(service.Monitor), service.Collected_Sec)
+    go graphite.Send(service.Name+".uptime", strconv.Itoa(service.Uptime), service.Collected_Sec)
+    go graphite.Send(service.Name+".children", strconv.Itoa(service.Children), service.Collected_Sec)
+    go graphite.Send(service.Name+".memory.percent", strconv.FtoaN(service.Memory.Percent,'g', -1, 2), service.Collected_Sec)
+    go graphite.Send(service.Name+".memory.percent_total", strconv.FtoaN(service.Memory.Percenttotal,'g', -1, 2), service.Collected_Sec)
+    go graphite.Send(service.Name+".memory.kilobyte", strconv.Itoa(service.Memory.Kilobyte), service.Collected_Sec)
+    go graphite.Send(service.Name+".memory.kylobytetotal", strconv.Itoa(service.Memory.Kilobytetotal), service.Collected_Sec)
+    go graphite.Send(service.Name+".cpu.percent", strconv.FtoaN(service.Cpu.Percent,'g', -1, 2), service.Collected_Sec)
+    go graphite.Send(service.Name+".cpu.percenttotal", strconv.FtoaN(service.Cpu.Percenttotal, 'g', -1, 2), service.Collected_Sec)
+  }
+}
+
+func (graphite *Graphite) Send(metric string, value string, timestamp int64) {
   buffer := bytes.NewBufferString("")
   fmt.Fprintf(buffer, "monit.%s %s %d\n", metric, value, timestamp)
-	client.Write(buffer.Bytes())
+  log.Println(buffer.String())
+	graphite.conn.Write(buffer.Bytes())
 }
 
 func MonitServer(w http.ResponseWriter, req *http.Request) {
@@ -104,23 +135,15 @@ func MonitServer(w http.ResponseWriter, req *http.Request) {
     log.Fatal(err)
   }
   for _, service := range monit.Service {
-    if service.Type == 5 {
-      continue
-    }
-    go SendToGraphite(service.Name+".status", strconv.Itoa(service.Status), service.Collected_Sec)
-    go SendToGraphite(service.Name+".monitor", strconv.Itoa(service.Monitor), service.Collected_Sec)
-    go SendToGraphite(service.Name+".uptime", strconv.Itoa(service.Uptime), service.Collected_Sec)
-    go SendToGraphite(service.Name+".children", strconv.Itoa(service.Children), service.Collected_Sec)
-    go SendToGraphite(service.Name+".memory.percent", strconv.FtoaN(service.Memory.Percent,'g', -1, 2), service.Collected_Sec)
-    go SendToGraphite(service.Name+".memory.percent_total", strconv.FtoaN(service.Memory.Percenttotal,'g', -1, 2), service.Collected_Sec)
-    go SendToGraphite(service.Name+".memory.kilobyte", strconv.Itoa(service.Memory.Kilobyte), service.Collected_Sec)
-    go SendToGraphite(service.Name+".memory.kylobytetotal", strconv.Itoa(service.Memory.Kilobytetotal), service.Collected_Sec)
-    go SendToGraphite(service.Name+".cpu.percent", strconv.FtoaN(service.Cpu.Percent,'g', -1, 2), service.Collected_Sec)
-    go SendToGraphite(service.Name+".cpu.percenttotal", strconv.FtoaN(service.Cpu.Percenttotal, 'g', -1, 2), service.Collected_Sec)
+    log.Println("sending service!")
+    serviceq <- &service
   }
 }
 
 func main(){
+  var graphite Graphite
+  go graphite.Setup("localhost:2003")
+
 	http.HandleFunc("/collector", MonitServer)
   err := http.ListenAndServe(":3005", nil)
 	if err != nil {
